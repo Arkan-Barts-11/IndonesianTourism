35d10fd799b6604d3103c25d2605d9df7d7fff4e
unknown <arkanbarts@gmail.com>
2024-06-30 16:52:48 +0700

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+function processTransaction() {
+    var nama = document.getElementById('nama').value;
+    var email = document.getElementById('email').value;
+    var tujuan = document.getElementById('tujuan').value;
+    
+    alert('Transaksi untuk ' + nama + ' dengan tujuan ' + tujuan + ' telah diproses.');
+}
